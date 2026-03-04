1db59e1def8dabc10206cd3074d2e0440fdc1750
danieldesouzagadelha-cmyk <danieldesouzagadelha@gmail.com>
2026-03-03 21:31:29 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,55 +1,20 @@
-from data_collector import (
-    get_games_today,
-    get_featured_odds,
-    extract_home_odd
-)
-from elo import get_rating, calculate_probability
-from telegram_bot import send_message
+import os
+import requests
 
-EDGE_THRESHOLD = 0.07  # 7%
+TOKEN = os.getenv("TELEGRAM_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
 
+print("TOKEN:", TOKEN)
+print("CHAT_ID:", CHAT_ID)
 
-def run():
-    print("🚀 BOT ELO INICIADO")
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 
-    games = get_games_today()
-    print(f"Jogos encontrados: {len(games)}")
+payload = {
+    "chat_id": CHAT_ID,
+    "text": "🚀 TESTE FINAL AGORA"
+}
 
-    for game in games[:20]:  # limita para não sobrecarregar
-        home = game["home"]
-        away = game["away"]
+response = requests.post(url, data=payload)
 
-        odds_data = get_featured_odds(game["id"])
-
-        if not odds_data:
-            continue
-
-        odd_home = extract_home_odd(odds_data)
-
-        if not odd_home:
-            continue
-
-        home_rating = get_rating(home)
-        away_rating = get_rating(away)
-
-        prob_model = calculate_probability(home_rating, away_rating)
-        prob_market = 1 / odd_home
-        edge = prob_model - prob_market
-
-        print(f"{home} vs {away} | Odd: {round(odd_home,2)} | Edge: {round(edge,4)}")
-
-        if edge >= EDGE_THRESHOLD:
-            message = (
-                f"📊 <b>ALERTA QUANTITATIVO</b>\n\n"
-                f"{home} vs {away}\n\n"
-                f"Modelo: {round(prob_model*100,2)}%\n"
-                f"Mercado: {round(prob_market*100,2)}%\n"
-                f"Odd: {round(odd_home,2)}\n"
-                f"Edge: {round(edge*100,2)}%"
-            )
-
-            send_message(message)
-
-
-if __name__ == "__main__":
-    run()
+print("STATUS:", response.status_code)
+print("RESPOSTA:", response.text)
